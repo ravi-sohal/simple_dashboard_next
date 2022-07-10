f5fe30ef140780b4609205db928c5c34915bd00f
ravi-sohal <94403292+ravi-sohal@users.noreply.github.com>
2022-07-10 14:55:24 +1200

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import {
+  Flowbite
+} from 'flowbite-react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Flowbite
+      // theme={{
+      //   theme: {
+      //     alert: {
+      //       color: {
+      //         primary: 'bg-primary'
+      //       }
+      //     }
+      //   }
+      // }}
+    >
+      <Component {...pageProps} />
+    </Flowbite>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import transactions from '../lib/transactions'
 import chart from '../lib/chart'
+import { DarkThemeToggle } from 'flowbite-react'
 import {
   Area,
   Bar,
@@ -8,11 +9,20 @@ import {
 } from '@ant-design/plots'
 
 export default function Index() {
-  const sales = transactions.dump(100)
-  const product = sales.map(s => s.product)
-  console.log(product)
+  const sales = transactions.dump(1000)
+  const products = [... new Set(sales.map(s => s.device))]
 
-  const datewise_sales = chart.prep_data(sales, 'date', 'count', 'product')
+  const datewise_sales = chart.prep_data(sales, 'count', 'date', 'name')
 
-  return <Column {...chart.get_config(datewise_sales)} />
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Retail Sales Analytics
+      </h1>
+      
+      <div className="grid grid-cols-1 md:grid-cols-1">
+        <Bar {...chart.get_config(datewise_sales)} />
+      </div>
+    </>
+  )
 }
