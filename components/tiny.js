0f83b7f63f2b14f713c6ba0bbda2b8d8d7efbb76
ravi-sohal <94403292+ravi-sohal@users.noreply.github.com>
2022-07-12 12:42:50 +1200

    cleanup

--- components/tiny.js
+++ components/tiny.js
@@ -1,5 +1,6 @@
 
 import {
+  Progress,
   TinyArea,
   TinyLine
 } from '@ant-design/plots'
@@ -10,61 +11,103 @@ const config = (data) => ({
   autoFit: true,
   smooth: false,
   width: 'auto',
+  tooltip: false,
   padding: [0, -26, -1],
   areaStyle: {
     fill: '#6692ea'
   }
 })
 
+const progress = (props) => {
+  const {
+    title,
+    percent
+  } = props
+
+  const progress_config = {
+    height: 30,
+    autoFit: false,
+    percent: percent,
+    color: ['#5B8FF9', '#E8EDF3'],
+  }
+
+  return (
+    <>
+      <p className="font-normal text-gray-700 dark:text-gray-200">{title}</p>
+      <div>
+        <Progress {...progress_config} />
+      </div>
+    </>
+  )
+}
+
 const area = (props) => {
+  const {
+    title,
+    text,
+    data
+  } = props
+
   return (
     <div className="w-full block bg-white rounded-md dark:bg-gray-800 ">
       <div className='p-6'>
-        <p className="font-normal text-gray-700 dark:text-gray-200">{props.title}</p>
+        <p className="font-normal text-gray-700 dark:text-gray-200">{title}</p>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {props.text}
+          {text}
         </h5>
       </div>
       <div>
-        <TinyArea {...config(props.data)} />
+        <TinyArea {...config(data)} />
       </div>
     </div>
   )
 }
 
 const line = (props) => {
+  const {
+    title,
+    text,
+    data
+  } = props
+
   return (
     <div className="w-full block bg-white rounded-md dark:bg-gray-800 ">
       <div className='p-6'>
-        <p className="font-normal text-gray-700 dark:text-gray-200">{props.title}</p>
+        <p className="font-normal text-gray-700 dark:text-gray-200">{title}</p>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {props.text}
+          {text}
         </h5>
       </div>
       <div>
-        <TinyLine {...config(props.data)} />
+        <TinyLine {...config(data)} />
       </div>
     </div>
   )
 }
 
 const text = (props) => {
+  const {
+    title,
+    text
+  } = props
+
   return (
     <div className="w-full block bg-white rounded-md dark:bg-gray-800 ">
       <div className='p-6'>
-        <p className="font-normal text-gray-700 dark:text-gray-200">{props.title}</p>
+        <p className="font-normal text-gray-700 dark:text-gray-200">{title}</p>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {props.text}
+          {text}
         </h5>
       </div>
     </div>
   )
 }
 
-const exports = {
+const tiny = {
+  progress,
   area,
   line,
   text
 }
 
-export default exports
+export default tiny
--- pages/index.js
+++ pages/index.js
@@ -2,72 +2,151 @@
 import Image from 'next/image'
 import generate from '../lib/generate'
 import count from '../lib/count'
+import group from '../lib/group'
 import table from '../components/table'
-import tiny from '../components/chart/tiny'
+import tiny from '../components/tiny'
+
+const logo = 'https://www.clipartmax.com/png/full/184-1849415_apple-logo-white-512x512-icon-apple-icon-png-white.png'
 
 export default function Index() {
-  const sales = generate.sales(1000)
+  
+  const sales = generate.sales(1500)
+
   const sales_by_day = count.by('date', sales)
+
+  const total_sales = sales.reduce((t, s) => t + s.price, 0)
+
+  const group_by_sales = group.by(['date'], sales)
+
+  const total_daily_sales = []
+  for (const [key, sale] of Object.entries(group_by_sales)) {
+    total_daily_sales.push(sale.reduce((t, s) => t + s.price, 0))
+  }
+
   const sales_by_devices = count.by('device', sales)
 
-  const best_seller = sales_by_devices.sort((a, b) => a.count - b.count)[0].name
+  const group_by_devices = group.by(['device'], sales)
+
+  const top_device = sales_by_devices
+    .sort((a, b) => a.count - b.count)[0].element
+
+  const top_device_daily_sales = count.by(
+      'date', 
+      group_by_devices[top_device]
+    )
+
+  const sales_by_agents = count.by('agent', sales)
+
+  const group_by_agents = group.by(['agent'], sales)
+
+  const top_agent = sales_by_agents
+    .sort((a, b) => a.count - b.count)[0].element
+
+  const top_agent_daily_sales = count.by(
+      'date', 
+      group_by_agents[top_agent]
+    )
+  
   return (
-    <div className='p-6 px-10'>
+    <div class="container m-auto p-4">
       <div className='mb-6'>
         <Image
-          src='https://www.clipartmax.com/png/full/184-1849415_apple-logo-white-512x512-icon-apple-icon-png-white.png'
-          className='max-w-9 h-11'
+          src={logo}
           width='40px'
           height='45px'
         />
       </div>
-      <div className='grid grid-cols-2 gap-4'>
-        <div>
-          <tiny.line
-            data={sales_by_day.map(s => s.count)}
-            title={'Sales'}
-            text={
-              'Daily Sales Average $' + Math.ceil(
-                sales_by_day
-                  .map(s => s.count)
-                  .reduce((t, a) => t + a) / sales_by_day.length
-              )
-            }
-          />
+      <div className='mb-4'>
+        <tiny.progress
+          title='Sales Target'
+          percent={0.55}
+        />
+      </div>
+      <div class="flex flex-row space-x-4">
+        <div class="flex w-full flex-col space-y-4">
+          <div>
+            <tiny.text
+              title={'Total Sales'}
+              text={
+                '$' + total_sales
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              }
+            />
+          </div>
+          <div>
+            <tiny.line
+              data={total_daily_sales}
+              title={'Average Daily Sales'}
+              text={
+                '$' + Math.ceil(
+                  total_daily_sales
+                    .reduce((t, a) => t + a) / total_daily_sales.length
+                )
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',') +
+                ' (' + Math.ceil(
+                  sales_by_day
+                    .map(s => s.count)
+                    .reduce((t, a) => t + a) / sales_by_day.length
+                ) + ')'
+              }
+            />
+          </div>
         </div>
-        <div>
-          {/* <Tiny.Line
-            data={
-              daily_count(
-                sales,
-                dates,
-                top_product
-              )
-            }
-            title={'Top Seller'}
-            text={top_product}
-          /> */}
+        <div class="flex w-full flex-col space-y-4">
+          <div>
+            <tiny.line
+              data={top_device_daily_sales.map(s => s.count)}
+              title={'Top Device'}
+              text={top_device}
+            />
+          </div>
+          <div>
+            <table.main
+              title='Top Devices'
+              headers={[
+                'Device',
+                'Sales'
+              ]}
+              columns={[
+                'element',
+                'count'
+              ]}
+              data={
+                sales_by_devices
+                  .sort((a, b) => b.count - a.count)
+                  .slice(0, 5)
+              }
+            />
+          </div>
         </div>
-        <div>
-          <table.main
-            title='Top Sellers'
-            headers={[
-              'Device',
-              'Sales'
-            ]}
-            columns={[
-              'element',
-              'count'
-            ]}
-            data={
-              sales_by_devices
-                .sort((a, b) => b.count - a.count)
-                .slice(0, 5)
-            }
-          />
-        </div>
-        <div>
-
+        <div class="flex w-full flex-col space-y-4">
+          <div>
+            <tiny.line
+              data={top_agent_daily_sales.map(s => s.count)}
+              title={'Top Agent'}
+              text={top_agent}
+            />
+          </div>
+          <div>
+            <table.main
+              title='Top Agents'
+              headers={[
+                'Device',
+                'Sales'
+              ]}
+              columns={[
+                'element',
+                'count'
+              ]}
+              data={
+                sales_by_agents
+                  .sort((a, b) => b.count - a.count)
+                  .slice(0, 5)
+              }
+            />
+          </div>
         </div>
       </div>
     </div>
